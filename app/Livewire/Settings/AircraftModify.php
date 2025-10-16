f66863f6a9151625382fd7d4851a843b40670dc3
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-16 14:46:42 +0700

    Aircraft modify page

---
+++ app/Livewire/Settings/AircraftModify.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Settings;
+
+use Livewire\Component;
+
+class AircraftModify extends Component
+{
+    public function render()
+    {
+        return view('livewire.settings.aircraft-modify');
+    }
+}
