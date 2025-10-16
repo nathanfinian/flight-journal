55fc074eb3d48181eaaacc91e29d706c0f67947c
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-16 14:48:21 +0700

    Use div instead of layout

--- resources/views/livewire/settings/aircraft.blade.php
+++ resources/views/livewire/settings/aircraft.blade.php
@@ -1,49 +1,47 @@
-<x-ui.layout>
-    <x-ui.layout.main>
-        <x-ui.layout.header class="flex justify-between dark:border-b-neutral-800 dark:bg-neutral-900">
-            <div class="flex items-center">
-                <x-ui.button
-                    wire:navigate.hover
-                    size="sm"
-                    variant="ghost"
-                    icon="arrow-left"
-                    :href="route('settings')"
-                    class="mr-6"
-                />
-                Aircraft List
+<div>
+    <x-ui.layout.header class="flex justify-between dark:border-b-neutral-800 dark:bg-neutral-900">
+        <div class="flex items-center">
+            <x-ui.button
+                wire:navigate.hover
+                size="sm"
+                variant="ghost"
+                icon="arrow-left"
+                :href="route('settings')"
+                class="mr-6"
+            />
+            Aircraft List
+    </div>
+        <div class="flex items-center">
+            <x-ui.button 
+                wire:navigate.hover
+                size="sm"
+                variant="outline"
+                icon="plus"
+                :href="route('settings.aircraft-modify')"
+            />
         </div>
-            <div class="flex items-center">
-                <x-ui.button 
-                    wire:navigate.hover
-                    size="sm"
-                    variant="outline"
-                    icon="plus"
-                    :href="route('settings.aircraft-modify')"
-                />
-            </div>
-        </x-ui.layout.header>
+    </x-ui.layout.header>
 
-        <table class="border-collapse border border-gray-400 w-full mt-4">
-            <thead>
-                <tr>
-                <th class="border border-gray-300 ...">State</th>
-                <th class="border border-gray-300 ...">City</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td class="border border-gray-300 ...">Indiana</td>
-                <td class="border border-gray-300 ...">Indianapolis</td>
-                </tr>
-                <tr>
-                <td class="border border-gray-300 ...">Ohio</td>
-                <td class="border border-gray-300 ...">Columbus</td>
-                </tr>
-                <tr>
-                <td class="border border-gray-300 ...">Michigan</td>
-                <td class="border border-gray-300 ...">Detroit</td>
-                </tr>
-            </tbody>
-        </table>
-    </x-ui.layout.main>
-</x-ui.layout>
+    <table class="border-collapse border border-gray-400 w-full mt-4">
+        <thead>
+            <tr>
+            <th class="border border-gray-300 ...">State</th>
+            <th class="border border-gray-300 ...">City</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td class="border border-gray-300 ...">Indiana</td>
+            <td class="border border-gray-300 ...">Indianapolis</td>
+            </tr>
+            <tr>
+            <td class="border border-gray-300 ...">Ohio</td>
+            <td class="border border-gray-300 ...">Columbus</td>
+            </tr>
+            <tr>
+            <td class="border border-gray-300 ...">Michigan</td>
+            <td class="border border-gray-300 ...">Detroit</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
