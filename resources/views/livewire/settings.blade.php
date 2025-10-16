15e05f5ff6b45dbdb34bc1f0c5fadab1e5cc9901
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-16 21:27:20 +0700

    Fixed formats on main settings page

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -1,59 +1,62 @@
 <div>
-    <x-ui.layout.header class="dark:border-b-neutral-800 dark:bg-neutral-900">
-        <h1 class="dark:border-b-neutral-800 dark:bg-neutral-900">Settings Page</h1>
-    </x-ui.layout.header>
-    
+    <x-ui.heading level="h1" size="xl">
+        Settings Page
+    </x-ui.heading>
+    <x-ui.separator class="my-2"/>
+
     <!-- Page content -->
-    <div class="p-6 mx-auto space-y-10">
-        <x-ui.link 
-            wire:navigate.hover
-            :href="route('settings.aircraft')"
-            variant="ghost"
-        >
-            <x-ui.card class="flex items-center justify-between mb-4" size="xl">
-                <span>Aircraft List</span>
-                <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
-            </x-ui.card>
-        </x-ui.link>
-        <x-ui.link 
-            wire:navigate.hover
-            :href="route('flight-journal')" 
-            variant="ghost"
-        >
-            <x-ui.card class="flex items-center justify-between mb-4" size="xl">
-                <span>Equipment List</span>
-                <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
-            </x-ui.card>
-        </x-ui.link>
-        <x-ui.link 
-            wire:navigate.hover
-            :href="route('flight-journal')" 
-            variant="ghost"
-        >
-            <x-ui.card class="flex items-center justify-between mb-4" size="xl">
-                <span>Airport List</span>
-                <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
-            </x-ui.card>
-        </x-ui.link>
-        <x-ui.link 
-            wire:navigate.hover
-            :href="route('flight-journal')" 
-            variant="ghost"
-        >
-            <x-ui.card class="flex items-center justify-between mb-4" size="xl">
-                <span>Routes List</span>
-                <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
-            </x-ui.card>
-        </x-ui.link>
-        <x-ui.link 
-            wire:navigate.hover
-            :href="route('flight-journal')" 
-            variant="ghost"
-        >
-            <x-ui.card class="flex items-center justify-between mb-4" size="xl">
-                <span>Regular Flights List</span>
-                <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
-            </x-ui.card>
-        </x-ui.link>
+    <div class="p-6">
+        <div class="w-full max-w-xl mx-auto space-y-4">
+            <x-ui.link 
+                wire:navigate.hover
+                :href="route('settings.aircraft')"
+                variant="ghost"
+            >
+                <x-ui.card class="flex items-center justify-between mb-4" size="xl">
+                    <span>Aircraft List</span>
+                    <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
+                </x-ui.card>
+            </x-ui.link>
+            <x-ui.link 
+                wire:navigate.hover
+                :href="route('flight-journal')" 
+                variant="ghost"
+            >
+                <x-ui.card class="flex items-center justify-between mb-4" size="xl">
+                    <span>Equipment List</span>
+                    <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
+                </x-ui.card>
+            </x-ui.link>
+            <x-ui.link 
+                wire:navigate.hover
+                :href="route('flight-journal')" 
+                variant="ghost"
+            >
+                <x-ui.card class="flex items-center justify-between mb-4" size="xl">
+                    <span>Airport List</span>
+                    <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
+                </x-ui.card>
+            </x-ui.link>
+            <x-ui.link 
+                wire:navigate.hover
+                :href="route('flight-journal')" 
+                variant="ghost"
+            >
+                <x-ui.card class="flex items-center justify-between mb-4" size="xl">
+                    <span>Routes List</span>
+                    <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
+                </x-ui.card>
+            </x-ui.link>
+            <x-ui.link 
+                wire:navigate.hover
+                :href="route('flight-journal')" 
+                variant="ghost"
+            >
+                <x-ui.card class="flex items-center justify-between mb-4" size="xl">
+                    <span>Regular Flights List</span>
+                    <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
+                </x-ui.card>
+            </x-ui.link>
+        </div>
     </div>
 </div>
