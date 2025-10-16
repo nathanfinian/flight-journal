76430a58ca6b15cf52dbbafe24135b9994e753b6
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-16 21:22:12 +0700

    Aircraft model modified

--- app/Models/Aircraft.php
+++ app/Models/Aircraft.php
@@ -6,8 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aircraft extends Model
 {
-    protected $table = 'aircraft';
-    protected $fillable = ['type_name', 'icao_code', 'iata_code', 'seat_capacity'];
+    protected $table = 'aircrafts';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'type_name', 
+        'icao_code', 
+        'iata_code', 
+        'seat_capacity'
+    ];
+
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'seat_capacity' => 'integer'
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'id'; //Assuming you are using 'id' as a unique field
+    }
 
     // public function equipment(): HasMany
     // {
