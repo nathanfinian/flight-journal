e656122f8fc3f10691b3d25be05b52e800c547bd
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-19 21:43:11 +0700

    Sheaf Modal component added

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './globals/theme.js'; /* By Sheaf.dev */
 
 import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import './globals/modals.js';
 
 // now you can register
 // components using Alpine.data(...) and
