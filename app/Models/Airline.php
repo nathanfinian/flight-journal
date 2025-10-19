8e3ebbde494081eeb9c461cc4706744b96e18ee5
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-19 13:22:40 +0700

    Airlines model with belongsToMany relation to the pivot table

---
+++ app/Models/Airline.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Airline extends Model
+{
+    protected $table = 'airlines';
+
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'icao_code',
+        'iata_code',
+        'country',
+        'created_by',
+        'updated_by'
+    ];
+
+    public function routes()
+    {
+        return $this->belongsToMany(Route::class, 'airline_route')->withTimestamps();
+    }
+}
