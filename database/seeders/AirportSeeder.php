af6a66f6fe3187f20af7ff56276c2eb062534958
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-20 15:35:45 +0700

    Include airport seeder

---
+++ database/seeders/AirportSeeder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class AirportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $now = Carbon::now();
+
+        DB::table('airports')->insert([
+            [
+                'iata' => 'CGK',
+                'icao' => 'WIII',
+                'city' => 'Jakarta',
+                'country' => 'Indonesia',
+                'tz' => 'Asia/Jakarta',
+                'created_at' => $now,
+                'updated_at' => $now,
+                'created_by' => 1,
+                'updated_by' => 1,
+            ],
+            [
+                'iata' => 'SUB',
+                'icao' => 'WARR',
+                'city' => 'Surabaya',
+                'country' => 'Indonesia',
+                'tz' => 'Asia/Jakarta',
+                'created_at' => $now,
+                'updated_at' => $now,
+                'created_by' => 1,
+                'updated_by' => 1,
+            ],
+            [
+                'iata' => 'DPS',
+                'icao' => 'WADD',
+                'city' => 'Denpasar (Bali)',
+                'country' => 'Indonesia',
+                'tz' => 'Asia/Makassar',
+                'created_at' => $now,
+                'updated_at' => $now,
+                'created_by' => 1,
+                'updated_by' => 1,
+            ],
+            [
+                'iata' => 'UPG',
+                'icao' => 'WAAA',
+                'city' => 'Makassar',
+                'country' => 'Indonesia',
+                'tz' => 'Asia/Makassar',
+                'created_at' => $now,
+                'updated_at' => $now,
+                'created_by' => 1,
+                'updated_by' => 1,
+            ],
+            [
+                'iata' => 'KNO',
+                'icao' => 'WIMM',
+                'city' => 'Medan (Kualanamu)',
+                'country' => 'Indonesia',
+                'tz' => 'Asia/Jakarta',
+                'created_at' => $now,
+                'updated_at' => $now,
+                'created_by' => 1,
+                'updated_by' => 1,
+            ],
+            [
+                'iata' => 'PKY',
+                'icao' => 'WAOP',
+                'city' => 'Palangkaraya',
+                'country' => 'Indonesia',
+                'tz' => 'Asia/Makassar',
+                'created_at' => $now,
+                'updated_at' => $now,
+                'created_by' => 1,
+                'updated_by' => 1,
+            ],
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            AirportSeeder::class,
+        ]);
+        $this->call([
             AircraftSeeder::class,
         ]);
         $this->call([
