133444ac7aac1182612eb5e0843dbea64159523f
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-20 15:59:59 +0700

    Changed airport link route

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -61,7 +61,7 @@
             </x-ui.link>
             <x-ui.link 
                 wire:navigate.hover
-                :href="route('settings.index')" 
+                :href="route('settings.airport')" 
                 variant="ghost"
             >
                 <x-ui.card class="flex items-center justify-between mb-4" size="xl">
