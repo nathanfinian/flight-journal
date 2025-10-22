cae53155590ce762e3d893e548d8d8380ffd92ed
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-22 17:17:02 +0700

    Branch migration

---
+++ database/migrations/2025_10_22_081020_create_branches_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            // Basic info
+            $table->string('name')->unique();
+            $table->enum('status', ['ACTIVE', 'INACTIVE'])->default('ACTIVE');
+
+            // Audit fields
+            $table->foreignId('created_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreignId('updated_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+
+            // Timestamps
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('branches');
+    }
+};
