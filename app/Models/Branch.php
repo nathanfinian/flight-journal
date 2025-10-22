6816dbc6c00c6c2022cd9e9c2cb4413a5a1b0ad7
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-22 17:16:47 +0700

    Branch model

---
+++ app/Models/Branch.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Branch extends Model
+{
+    protected $table = 'branches';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'name',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'id'; //Assuming you are using 'id' as a unique field
+    }
+}
