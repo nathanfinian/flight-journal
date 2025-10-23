ec16fc859e4b1c39fb9bc77663b412f507ec8603
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-23 21:59:00 +0700

    AirlineRoute, Pivot table has a model now

---
+++ app/Models/AirlineRoute.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AirlineRoute extends Model
+{
+    protected $table = 'airline_route';
+
+    protected $fillable = [
+        'airport_route_id',
+        'airline_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function airline()
+    {
+        return $this->belongsTo(Airline::class);
+    }
+
+    public function airportRoute()
+    {
+        return $this->belongsTo(AirportRoute::class, 'airport_route_id');
+    }
+
+    public function scheduledFlights()
+    {
+        return $this->hasMany(ScheduledFlights::class, 'airline_route_id');
+    }
+}
