19c21758db420d8c60b83d716da69b3a5cada793
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-23 09:37:58 +0700

    Use new logo

--- resources/views/components/app/logo.blade.php
+++ resources/views/components/app/logo.blade.php
@@ -1,11 +1,11 @@
 <div class="flex items-center gap-2">
     <a data-slot="button"
        class="dark:hover:bg-white/6 text-primary-brand bg-base-200/6 relative inline-flex h-[32px] w-[32px] items-center justify-center gap-2 whitespace-nowrap rounded-[3px] text-sm font-medium hover:bg-neutral-200"
-       href="/">
+       href="/dashboard">
         <img src="{{ asset('img/logo.png') }}" alt="Logo" class="h-10 w-auto" />
     </a>
     <a class="inline-flex items-center"
-       href="{{ route('home') }}"
+       href="{{ route('dashboard') }}"
        wire:navigate>
         <h1 class='text-primary-brand text-lg font-bold leading-8'>MCA | Journal</h1>
     </a>
