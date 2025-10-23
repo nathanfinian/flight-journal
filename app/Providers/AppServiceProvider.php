17cebe23185273fbd5332debdf13a2d3fcf38150
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-23 09:37:38 +0700

    Added validator for username to be 3-20 in length and letter or numbers only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // In a service provider boot() method:
+        Validator::extend('username', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/^[A-Za-z0-9_]{3,20}$/', $value);
+        });
+
+        Validator::replacer('username', function ($message, $attribute, $rule, $parameters) {
+            return 'The ' . $attribute . ' may only contain letters, numbers, and underscores, and must be 3–20 characters long.';
+        });
     }
 }
