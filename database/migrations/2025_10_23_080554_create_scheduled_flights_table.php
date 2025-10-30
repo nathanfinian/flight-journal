e7201fd94c5259b11fddd34d8d914aeed4a7d2af
Nathanael Finian <69637127+nathanfinian@users.noreply.github.com>
2025-10-30 19:40:53 +0700

    Adjusted scheduled_flights table, redundancy and column changes

--- database/migrations/2025_10_23_080554_create_scheduled_flights_table.php
+++ database/migrations/2025_10_23_080554_create_scheduled_flights_table.php
@@ -13,21 +13,20 @@ return new class extends Migration
     {
         Schema::create('scheduled_flights', function (Blueprint $table) {
             $table->id();
-            $table->string('flight_no', 20);  // ID-6200
+            $table->string('flight_no', 10)->unique();  // ID-6200
             $table->foreignId('airline_route_id')->constrained('airport_routes')
                 ->cascadeOnUpdate()->restrictOnDelete();
-            $table->foreignId('equipment_id')->constrained('equipments')
-                ->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('equipment_id')
+                ->nullable()
+                ->constrained('equipments')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
             $table->foreignId('branch_id')->constrained('branches')
                 ->cascadeOnUpdate()->restrictOnDelete();
 
-            $table->date('service_date');     // 2025-07-01
             // Optional planned times
             $table->time('sched_dep')->nullable();
             $table->time('sched_arr')->nullable();
-
-            $table->unique(['flight_no', 'service_date'], 'uniq_flight_instance');
-            $table->index(['airline_route_id', 'service_date'], 'idx_route_date');
 
             $table->foreignId('created_by')->nullable()
                 ->constrained('users')->nullOnDelete()->cascadeOnUpdate();
