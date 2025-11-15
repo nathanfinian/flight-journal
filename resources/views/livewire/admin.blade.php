ea947fdf57051ab0263027d908ef7b3be225e428
Nathanael Finian <nathanfinian@gmail.com>
2025-11-15 12:05:20 +0700

    Display admin page to access roles and users

---
+++ resources/views/livewire/admin.blade.php
@@ -0,0 +1,38 @@
+<div>
+    <x-ui.heading level="h1" size="xl">
+        Admin Page
+    </x-ui.heading>
+    <x-ui.separator class="my-2"/>
+
+    <!-- Page content -->
+    <div class="p-6">
+        <div class="w-full max-w-xl mx-auto space-y-4">
+            <x-ui.link 
+                wire:navigate.hover
+                :href="route('admin.users')" 
+                variant="ghost"
+            >
+                <x-ui.card class="flex items-center justify-between mb-4" size="xl">
+                    <div class="flex">
+                        <x-ui.icon name="users" variant="light" class="text-gray-800 dark:text-white mr-2"/>
+                        <span>users</span>
+                    </div>
+                    <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
+                </x-ui.card>
+            </x-ui.link>
+            <x-ui.link 
+                wire:navigate.hover
+                {{-- :href="route('admin.roles')"  --}}
+                variant="ghost"
+            >
+                <x-ui.card class="flex items-center justify-between mb-4" size="xl">
+                    <div class="flex">
+                        <x-ui.icon name="ps:user-square" variant="light" class="text-gray-800 dark:text-white mr-2"/>
+                        <span>roles</span>
+                    </div>
+                    <x-ui.icon name="arrow-up-right" class="text-gray-800 dark:text-white size-4" />
+                </x-ui.card>
+            </x-ui.link>
+        </div>
+    </div>
+</div>
