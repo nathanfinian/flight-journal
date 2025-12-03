e0b219d44b41cd408e0568b32ee2015324459fb7
Nathanael Finian <nathanfinian@gmail.com>
2025-12-03 15:51:53 +0700

    Export Controller, prepend space, added methods to prepend space, readableDate, data query for print

--- app/Http/Controllers/Export/FlightExportController.php
+++ app/Http/Controllers/Export/FlightExportController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Exports\FlightsExport;
 use App\Http\Controllers\Controller;
 use Maatwebsite\Excel\Facades\Excel;
+use App\Models\Flight;
 
 class FlightExportController extends Controller
 {
@@ -19,8 +20,8 @@ class FlightExportController extends Controller
         $this->dateFrom = $request->dateFrom;
         $this->dateTo = $request->dateTo;
         $this->branch = $request->branchName;
-        $this->airline = $request->airlineName;
-        
+        $this->airline = $this->prependSpace($request->airlineName);
+
         return Excel::download(
             new FlightsExport(
                 $request->dateFrom,
@@ -28,7 +29,7 @@ class FlightExportController extends Controller
                 $request->branch,
                 $request->airline
             ),
-            $this->branch . ' ' . $this->dateFrom . ' sampai ' . $this->dateTo . ' ' . $this->airline . ' Flights.xlsx'
+            $this->branch . ' ' . $this->dateFrom . ' sampai ' . $this->dateTo . $this->airline . ' Flights.xlsx'
         );
     }
 
@@ -37,7 +38,7 @@ class FlightExportController extends Controller
         $this->dateFrom = $request->dateFrom;
         $this->dateTo = $request->dateTo;
         $this->branch = $request->branchName;
-        $this->airline = $request->airlineName;
+        $this->airline = $this->prependSpace($request->airlineName);
 
         return Excel::download(
             new FlightsExport(
@@ -48,5 +49,93 @@ class FlightExportController extends Controller
             ),
             $this->branch . ' ' . $this->dateFrom . ' sampai ' . $this->dateTo . ' ' . $this->airline . ' Flights.pdf',
              \Maatwebsite\Excel\Excel::MPDF);
+    }
+
+    public function print(Request $request)
+    {
+        $dateFrom = $request->dateFrom;
+        $dateTo = $request->dateTo;
+
+        $branch = $request->branch;
+        $airline = $request->airline;
+        $airlineName = $request->airlineName
+            ? ' - ' . $request->airlineName
+            : '';
+        $branchName = $request->branchName
+            ? 'Cabang ' . $request->branchName
+            : '';
+
+        $data = $this->getFlights($branch, $airline, $dateFrom, $dateTo);
+
+        $dateFrom = $this->readableDate($dateFrom);
+        $dateTo = $this->readableDate($dateTo);
+
+        return view('print.flight-history', [
+            'flights'     => $data,
+            'branch'      => $branchName,
+            'airline'     => $airlineName,
+            'dateFrom'    => $dateFrom,
+            'dateTo'      => $dateTo,
+        ]);
+    }
+
+    private function readableDate(?string $value): string
+    {
+        if (!$value) {
+            return '';
+        }
+
+        return \Carbon\Carbon::parse($value)
+            ->locale('id')
+            ->translatedFormat('d M Y');
+    }
+
+    private function prependSpace(?string $value): string
+    {
+        return $value ? ' ' . $value : '';
+    }
+
+    private function getFlights($branch, $airline, $from, $to)
+    {
+        return Flight::query()
+            ->with([
+                'branch:id,name',
+                'originEquipment:id,registration',
+                'departureEquipment:id,registration',
+                'originAirlineRoute.airline:id,name',
+                'originAirlineRoute.airportRoute.origin:id,iata',
+                'originAirlineRoute.airportRoute.destination:id,iata',
+                'departureAirlineRoute.airportRoute.origin:id,iata',
+                'departureAirlineRoute.airportRoute.destination:id,iata',
+            ])
+            // Filter branch
+            ->when($branch, fn($q) => $q->where('branch_id', $branch))
+
+            // Filter airline
+            ->when(
+                $airline,
+                fn($q) =>
+                $q->whereHas(
+                    'originAirlineRoute',
+                    fn($r) => $r->where('airline_id', $airline)
+                )
+            )
+
+            // DATE FILTERS
+            ->when(
+                $from && $to,
+                fn($q) => $q->whereBetween('service_date', [$from, $to])
+            )
+            ->when(
+                $from && !$to,
+                fn($q) => $q->whereDate('service_date', '>=', $from)
+            )
+            ->when(
+                !$from && $to,
+                fn($q) => $q->whereDate('service_date', '<=', $to)
+            )
+
+            ->orderBy('service_date', 'asc')
+            ->get();
     }
 }
