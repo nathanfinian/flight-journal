eed011cc07eff6701d283f2af9fe6964b0936df3
Nathanael Finian <nathanfinian@gmail.com>
2025-12-04 13:59:54 +0700

    FlightType Model

---
+++ app/Models/FlightType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+
+class FlightType extends Model
+{
+    protected $table = 'flight_types';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'name',
+        'type_code',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $model->created_by = Auth::id();
+            $model->updated_by = Auth::id();
+        });
+
+        static::updating(function ($model) {
+            $model->updated_by = Auth::id();
+        });
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function scheduledFlights()
+    {
+        return $this->hasMany(ScheduledFlights::class, 'airline_route_id');
+    }
+
+    public function flights()
+    {
+        return $this->hasMany(Flight::class, 'airline_route_id');
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'branch_id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'id'; //Assuming you are using 'id' as a unique field
+    }
+}
