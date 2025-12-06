f5365012600515b795cdd9e742ed0235c65902a0
Nathanael Finian <nathanfinian@gmail.com>
2025-12-06 15:24:24 +0700

    Airline Rate model

---
+++ app/Models/AirlineRate.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+
+class AirlineRate extends Model
+{
+    protected $table = 'airline_rates';
+
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'airline_id',
+        'charge_name',
+        'charge_code',
+        'ground_fee',
+        'cargo_fee',
+    ];
+    
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $model->created_by = Auth::id();
+            $model->updated_by = Auth::id();
+        });
+
+        static::updating(function ($model) {
+            $model->updated_by = Auth::id();
+        });
+    }
+
+    public function airline()
+    {
+        return $this->belongsTo(Airline::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'id'; //Assuming you are using 'id' as a unique field
+    }
+}
