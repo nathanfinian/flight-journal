8791bb7eebc8517ad485482e9838ec2b5b9c9b39
Nathanael Finian <nathanfinian@gmail.com>
2026-03-12 20:00:35 +0700

    GSE GPU Detail Model, change to use recap_id instead of invoice_id

--- app/Models/GseGpuDetail.php
+++ app/Models/GseGpuDetail.php
@@ -10,13 +10,13 @@ class GseGpuDetail extends Model
     protected $table = 'gse_gpu_details';
 
     protected $fillable = [
-        'gse_invoice_id',
+        'gse_recap_id',
         'start_time',
         'end_time',
     ];
 
-    public function invoice(): BelongsTo
+    public function recap(): BelongsTo
     {
-        return $this->belongsTo(Invoice_gse::class, 'gse_invoice_id');
+        return $this->belongsTo(GseRecap::class, 'gse_recap_id');
     }
 }
