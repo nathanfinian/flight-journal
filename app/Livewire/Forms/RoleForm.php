dc9364b79a4acf34b81187030d12c038e9b95949
Nathanael Finian <nathanfinian@gmail.com>
2026-06-01 16:06:48 +0700

    Role validation fixes

--- app/Livewire/Forms/RoleForm.php
+++ app/Livewire/Forms/RoleForm.php
@@ -4,15 +4,20 @@ namespace App\Livewire\Forms;
 
 use Livewire\Form;
 use App\Models\Role;
-use Livewire\Attributes\Validate;
 
 class RoleForm extends Form
 {
-    #[Validate('required|string|min:3|max:80')]
     public string $name = '';
 
-    #[Validate('required|string|min:3|max:80')]
     public string $label = '';
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:3', 'max:80'],
+            'label' => ['required', 'string', 'min:3', 'max:80'],
+        ];
+    }
 
     public function fillFromModel(Role $role)
     {
