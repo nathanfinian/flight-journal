422cb5feb0bd2550f17a610b776faa7913c36dc1
Nathanael Finian <nathanfinian@gmail.com>
2026-06-19 14:02:46 +0700

    Revert Back, now only admins can edit

--- app/Livewire/GseInventoryItems/Index.php
+++ app/Livewire/GseInventoryItems/Index.php
@@ -21,7 +21,7 @@ class Index extends Component
 
     public function mount(): void
     {
-        $this->canOpenEditPage = in_array(Auth::user()?->role?->name, ['admin', 'finance'], true);
+        $this->canOpenEditPage = in_array(Auth::user()?->role?->name, ['admin'], true);
     }
 
     public function openEdit(int $id)
