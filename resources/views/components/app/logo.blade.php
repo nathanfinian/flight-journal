ca696baeb99f767d7347f27e4faecf46b8c164d9
Nathanael Finian <nathanfinian@gmail.com>
2026-06-22 22:44:39 +0700

    Deprecated, adjusted

--- resources/views/components/app/logo.blade.php
+++ resources/views/components/app/logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-2">
     <a data-slot="button"
-       class="dark:hover:bg-white/6 text-primary-brand bg-base-200/6 relative inline-flex h-[32px] w-[32px] items-center justify-center gap-2 whitespace-nowrap rounded-[3px] text-sm font-medium hover:bg-neutral-200"
+       class="dark:hover:bg-white/6 text-primary-brand bg-base-200/6 relative inline-flex h-8 w-8 items-center justify-center gap-2 whitespace-nowrap rounded-[3px] text-sm font-medium hover:bg-neutral-200"
        href="/dashboard">
         <img src="{{ asset('img/logo.png') }}" alt="Logo" class="h-10 w-auto" />
     </a>
